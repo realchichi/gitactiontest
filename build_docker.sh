8723d30dd1283ab65a0099f038a1b9b9847a208d
wachirapong-n <wachirapong_n@cmu.ac.th>
2024-01-31 22:38:24 +0700

    edit docker

---
+++ build_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+[ -e "$PWD"/.env.dev ] && . "$PWD"/.env.dev
+app="docker.project"
+docker build -t ${app} .
+docker run -p 56700:8000 -d \
+  --name=${app} \
+  -e FLASK_DEBUG=${FLASK_DEBUG} \
+  -v $PWD:/flask_app ${app}
