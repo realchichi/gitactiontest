99ac72564453cdef08685b3cc5442cbed0b5ac75
mykhbas <wolfbaszxc@gmail.com>
2024-02-05 21:15:47 +0700

    facebook

--- flask/app/models/user.py
+++ flask/app/models/user.py
@@ -1,20 +1,20 @@
-from app import db
-from sqlalchemy_serializer import SerializerMixin
+# from app import db
+# from sqlalchemy_serializer import SerializerMixin
 
-class User(db.Model, SerializerMixin):
-	__tablename__ = "users"
+# class User(db.Model, SerializerMixin):
+# 	__tablename__ = "users"
 
-	id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50))
-    password = db.Column(db.String(50))
+# 	id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(50))
+#     password = db.Column(db.String(50))
     
 
 
-    def __init__(self, email, password):
-    	self.email = email
-    	self.password = password
+#     def __init__(self, email, password):
+#     	self.email = email
+#     	self.password = password
 
 
-    def update(self, email, password):
-       	self.email = email
-    	self.password = password
+#     def update(self, email, password):
+#        	self.email = email
+#     	self.password = password
--- flask/app/views.py
+++ flask/app/views.py
@@ -3,14 +3,14 @@ import json
 import http.client
 # from urllib.request import urlopen
 from app.forms import forms
-
+# from flask_wtf import FlaskForm
 
 import psycopg2
 from flask import (jsonify, render_template,request)
 from app import app
-from app import db
-from sqlalchemy.sql import text
-from app.models.user import User
+# from app import db
+# from sqlalchemy.sql import text
+# from app.models.user import User
 
 READY = False
 
@@ -30,10 +30,40 @@ def form():
     
        
     return render_template('login.html', form=form)
-@app.route('/')
-def home():
-    return app.send_static_file('login.html')
+# @app.route('/')
+# def home():
+#     # if request.method == 'POST':
+#     #     result = request.form.to_dict()
+#     #     validated = True
+#     #     validated_dict = dict()
+#     #     id_ = result.get('id', '')
+#     #     valid_keys = ['email', 'password']
 
+
+#     #     # validate the input
+#     #     for key in result:
+#     #         # screen of unrelated inputs
+#     #         value = result[key].strip()
+#     #         if not value or value == 'undefined':
+#     #             validated = False
+#     #             break
+#     #         validated_dict[key] = value
+
+
+#     #     if validated:
+#     #         # if there is no id: create a new contact entry
+
+#     #         if not id_:
+#     #             entry = Contact(**validated_dict)
+#     #             db.session.add(entry)
+#     #         # if there is an id already: update the contact entry
+#     #         else:
+#     #             contact = Contact.query.get(id_)
+#     #             contact.update(**validated_dict)
+
+
+#     #         db.session.commit()
+#     return app.send_static_file('login.html')
 
 
 @app.route('/db')
@@ -48,7 +78,7 @@ def db_connection():
 
 @app.route("/home")
 def home():
-    return render_template("login.html")
+    return app.send_static_file("login.html")
 
 
 @app.route("/camera")
@@ -56,40 +86,40 @@ def camera_access():
     return render_template("camera.html")
 
 
-@app.route("/api")
-def call_api():
-    if READY:
-      with open('static/img/photo1.jpg', 'rb') as file:
-          images = [base64.b64encode(file.read()).decode('ascii')]
+# @app.route("/api")
+# def call_api():
+#     if READY:
+#       with open('static/img/photo1.jpg', 'rb') as file:
+#           images = [base64.b64encode(file.read()).decode('ascii')]
 
-      type_iden = ["health_assessment","identification"]
+#       type_iden = ["health_assessment","identification"]
 
-      url_iden = "https://plant.id/api/v3/" + type_iden[1]
-      DETAILS = "common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,edible_parts,watering,propagation_methods"
-      q_iden = "?details=" + DETAILS +"&language=en"
-      url_iden += q_iden
+#       url_iden = "https://plant.id/api/v3/" + type_iden[1]
+#       DETAILS = "common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,edible_parts,watering,propagation_methods"
+#       q_iden = "?details=" + DETAILS +"&language=en"
+#       url_iden += q_iden
 
-      payload = json.dumps({
-        "images": ["data:image/jpg;base64," + images[0]],
-        "similar_images": "true"
-      })
-      headers = {
-        'Api-Key': 'XbcsHOYrpQJBei7BNsrP7TeXUyerkYd1SpqRVAfSgq2T9lIZbu',
-        'Content-Type': 'application/json'
-      }
+#       payload = json.dumps({
+#         "images": ["data:image/jpg;base64," + images[0]],
+#         "similar_images": "true"
+#       })
+#       headers = {
+#         'Api-Key': 'XbcsHOYrpQJBei7BNsrP7TeXUyerkYd1SpqRVAfSgq2T9lIZbu',
+#         'Content-Type': 'application/json'
+#       }
 
-      response = requests.request("POST", url_iden, headers=headers, data=payload)
+#       response = requests.request("POST", url_iden, headers=headers, data=payload)
 
-  # list_data = get_data(response)
+#   # list_data = get_data(response)
 
-  raw_data = read_file("app/sandbox/Tle_sandbox/fake_data.txt")
-  fake_data = list(map(lambda x : get_data(eval(x)), raw_data))
-  list_data = 
-  db_plant = 
-  # print(fake_data)
+#       raw_data = read_file("app/sandbox/Tle_sandbox/fake_data.txt")
+#       fake_data = list(map(lambda x : get_data(eval(x)), raw_data))
+#   # list_data = 
+#   # db_plant = 
+#   # print(fake_data)
 
 
-  return render_template("temp.html", data=fake_data)
+#   return render_template("temp.html", data=fake_data)
 
 @app.route("/user")
 def user():
@@ -108,9 +138,9 @@ def validate_user():
 
 
 def get_data(res):
-  is_plant = res["result"]["is_plant"]["probability"]
-  name = res["result"]["classification"]["suggestions"]
-  dict_val = {}
+    is_plant = res["result"]["is_plant"]["probability"]
+    name = res["result"]["classification"]["suggestions"]
+    dict_val = {}
   # name                : latin name
   # probability         : prob that is plant
   # similar_img         : similar image
@@ -122,22 +152,22 @@ def get_data(res):
   # img                 : image of this plant
   # watering            : watering how wet environment the plant prefers(1 = dry, 2 = medium, 3 = wet)
   # propagation_methods : propagation method
-  list_data = []
-  for val in name:
-    dict_val["id"] = val["id"]
-    dict_val["name"] = val["name"]
-    dict_val["probability"] = val["probability"]
-    dict_val["similar_img"] = val["similar_images"]
-    dict_val["common_name"] = val["details"]["common_names"]
-    dict_val["taxonomy"] = val["details"]["taxonomy"]
-    dict_val["url"] = val["details"]["url"]
-    dict_val["description"] = val["details"]["description"]["value"]
-    dict_val["synonyms"] = val["details"]["synonyms"]
-    dict_val["img"] = val["details"]["image"]["value"]
-    dict_val["watering"] = val["details"]["watering"]
-    dict_val["propagation_methods"] = val["details"]["propagation_methods"]
-    list_data.append(dict_val)
-  return list_data
+    list_data = []
+    for val in name:
+      dict_val["id"] = val["id"]
+      dict_val["name"] = val["name"]
+      dict_val["probability"] = val["probability"]
+      dict_val["similar_img"] = val["similar_images"]
+      dict_val["common_name"] = val["details"]["common_names"]
+      dict_val["taxonomy"] = val["details"]["taxonomy"]
+      dict_val["url"] = val["details"]["url"]
+      dict_val["description"] = val["details"]["description"]["value"]
+      dict_val["synonyms"] = val["details"]["synonyms"]
+      dict_val["img"] = val["details"]["image"]["value"]
+      dict_val["watering"] = val["details"]["watering"]
+      dict_val["propagation_methods"] = val["details"]["propagation_methods"]
+      list_data.append(dict_val)
+    return list_data
 
 
 
