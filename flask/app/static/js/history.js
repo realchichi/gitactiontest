95f1818f836946de2aeec5605848936d1f19d32d
realchichi <nsrixxns@gmail.com>
2024-03-03 05:43:38 +0700

    history UI

--- flask/app/static/js/history.js
+++ flask/app/static/js/history.js
@@ -16,14 +16,14 @@ function createbox(data){
     for(let i=0;i<data.length;i++){
         if(!(data[i]["removed_date"])){
             console.log(data[i])
-            var x ='<div class="col-md-4">' +
-            '<div id="box_add_history" class="card">' +
+            var x ='<div class="col-3" id="box_add_history">' +
+            '<div class="card">' +
                 '<button id="plant_ifo" class="card-img-top">' +
                     '<img id="identified_img" src="' + data[i]["identified_img"].slice(4) + '" max-width="100" onclick="redirectToResult(this)">' +
                 '</button>' +
                 '<div id="data" class="card-body">' +
-                    '<p id="identified_date" class="card-text">identified_date : ' + data[i]["identified_date"] + '</p>' +
-                    '<a id="delete_button" class="btn" value="' + [data[i]["id"], data[i]["account_id"]] + '" onclick="handleClickDelete(this)">🗑️</a>' +
+                    '<p id="identified_date" class="card-text">Identified date :<br>'+ data[i]["identified_date"] + '</p>' +
+                    '<a id="delete_button" class="btn" value="' + [data[i]["id"], data[i]["account_id"]] + '" onclick="handleClickDelete(this)"><i class="fa-solid fa-trash-can"></a>' +
                 '</div>' +
             '</div>' +
         '</div>';
